e7f85f11536970fff6339b9553c86389b93dca44
Emanuel Huber <emanuel.tesv@gmail.com>
2021-05-09 03:57:33 -0300

    Added udpipe train app

---
+++ dante_parser/apps/train_udpipe.py
@@ -0,0 +1,18 @@
+import argparse
+
+from dante_parser.data import get_datasets, load_splitted_data
+from dante_parser.parser.udpipe import train_udpipe
+
+
+def main():
+    parser = argparse.ArgumentParser("Trains Udpipe Model")
+    parser.add_argument("--datasets", type=str, default="dante_01")
+    args = parser.parse_args()
+
+    datasets = args.datasets.split(" ")
+    train, val, test = load_splitted_data(datasets)
+
+    train_udpipe(train, val, "mymodel.model")
+
+if __name__ == "__main__":
+    main()
