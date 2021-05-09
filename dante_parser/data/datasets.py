3a8fe5d2d8e9166811422fa1d72a94be21dcac31
Emanuel Huber <emanuel.tesv@gmail.com>
2021-05-08 21:28:41 -0300

    Added get_datasets test

--- dante_parser/data/datasets.py
+++ dante_parser/data/datasets.py
@@ -2,8 +2,9 @@ import os
 
 import dante_parser
 
+
 def get_datasets():
-    """ Return list of supported datasets """
+    """ Return list of supported datasets with corresponding path and filetypes """
     
     base_path = os.path.dirname(dante_parser.__file__)
     base_path = os.path.join(base_path, "datasets")
@@ -19,6 +20,16 @@ def get_datasets():
             },
             "test": {
                 "path": os.path.join(base_path, "bosque", "pt_bosque-ud-test.txt"),
+                "filetype": "conllu"
+            }
+        },
+        "dante_01": {
+            "original": {
+                "path": os.path.join(base_path, "dante_01", "tweets_stocks.csv"),
+                "filetype": "csv"
+            },
+            "1a150": {
+                "path": os.path.join(base_path, "dante_01", "1a150.csv"),
                 "filetype": "conllu"
             }
         }
---
+++ dante_parser/data/tests/test_datasets.py
@@ -0,0 +1,16 @@
+from dante_parser.data import get_datasets
+
+
+def test_get_datasets():
+
+    datasets = get_datasets()
+
+    assert len(datasets.keys()) > 0
+    
+    for key, data in datasets.items():
+        for set_key, set_value in data.items():
+
+            assert "path" in set_value.keys()
+            assert "filetype" in set_value.keys()
+            assert set_value["filetype"] in ["csv", "conllu"]
+
