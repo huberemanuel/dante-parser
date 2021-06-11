27de2462aba6d100b9b5b3b8a6aea0d63a1c353f
Emanuel Huber <emanuel.tesv@gmail.com>
2021-06-10 21:45:47 -0300

    Updated train_udpipe model name

--- dante_parser/apps/train_udpipe.py
+++ dante_parser/apps/train_udpipe.py
@@ -9,17 +9,18 @@ def main():
     parser.add_argument("--datasets", type=str, default="bosque dante_01")
     parser.add_argument("--all_data", default=False, action="store_true",
                         help="Concatenate all sets (train, val, test) to train")
+    parser.add_argument("--out_name", type=str, default="mymodel.model", 
+                        help="Name of the output model file")
     args = parser.parse_args()
 
-    model_name = "mymodel.model"
     datasets = args.datasets.split(" ")
     if args.all_data:
         train = load_data(datasets)
-        train_udpipe(train, [], model_name)
+        train_udpipe(train, [], args.out_name)
     else:
         train, val, test = load_splitted_data(datasets)
 
-        train_udpipe(train, val, model_name)
+        train_udpipe(train, val, args.out_name)
 
 if __name__ == "__main__":
     main()
